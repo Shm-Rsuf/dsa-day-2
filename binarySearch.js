3d6e08e26988bac318c790edbfa73ccc8700d916
shm_usuf <shmusuf@gmail.com>
2023-01-21 10:46:45 +0600

    linear-search

--- binarySearch.js
+++ binarySearch.js
@@ -6,6 +6,14 @@ function binarySearch(arr, target) {
     let mid = Math.floor((left + right) / 2);
     if (target === arr[mid]) {
       return mid;
+    } else if (target > arr[mid]) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
     }
   }
+  return -1;
 }
+
+console.log(binarySearch([1, 3, 4, 8, 11], 11)); //4
+console.log(binarySearch([1, 3, 4, 8, 11], 14)); //-1
--- linearSearch.js
+++ linearSearch.js
@@ -1,7 +1,7 @@
 // given an array, [1, 5, 14, 7, 9]
 // a target element
 
-function linearSearch(arr, target) {
+/* function linearSearch(arr, target) {
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] === target) {
       return i;
@@ -11,6 +11,20 @@ function linearSearch(arr, target) {
 }
 
 console.log(linearSearch([1, 5, 14, 7, 9], 7)); //3
-console.log(linearSearch([1, 5, 14, 7, 9], 6)); //-1
+console.log(linearSearch([1, 5, 14, 7, 9], 6)); //-1 */
 
 //O(n)==>linear complexcity
+
+/*************************************************/
+function linearsearch(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === target) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+console.log(linearsearch([4, 2, 7, 1, 8, 10], 7));
+console.log(linearsearch([4, 2, 7, 1, 8, 10], 25));
+console.log(linearsearch([4, 2, 7, 1, 8, 10], 8));
